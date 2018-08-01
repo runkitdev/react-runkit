726bf418d40115c77fa0bfe8aaf4519c5f0cdee0
tomholford <tomholford@users.noreply.github.com>
2018-07-31 17:04:06 -0700

    Ensure string

--- src/urlencode.js
+++ src/urlencode.js
@@ -1,8 +1,10 @@
 export default function(text) {
-  return encodeURIComponent(text).replace(/!/g, '%21')
-                                 .replace(/'/g, '%27')
-                                 .replace(/\(/g, '%28')
-                                 .replace(/\)/g, '%29')
-                                 .replace(/\*/g, '%2A')
-                                 .replace(/%20/g, '+');
+  const str = text.toString();
+
+  return encodeURIComponent(str).replace(/!/g, '%21')
+                                .replace(/'/g, '%27')
+                                .replace(/\(/g, '%28')
+                                .replace(/\)/g, '%29')
+                                .replace(/\*/g, '%2A')
+                                .replace(/%20/g, '+');
 }
